501183214f0f65b5dde1660535cb81cdf39216f5
Hunterbender <121184836+Hunterbender@users.noreply.github.com>
2023-03-20 15:52:15 +0100

    Changed stage Title

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.example.activities {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.example.activities to javafx.fxml;
     exports com.example.activities;
 }
